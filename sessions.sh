cb96f2e01fed434e2b2ef865c68d8de485893095
Manuel Šarić <manujelko@gmail.com>
2026-01-01 16:32:36 +0100

    Tmux stuff

---
+++ sessions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIRS=(
+    "$HOME/code"
+)
+
+SELECTED=$(
+    {
+        find "${DIRS[@]}" -maxdepth 1 -type d
+        printf '%s\n' \
+            "$HOME/.config/tmux" \
+            "$HOME/.config/nvim"
+    } | sed "s|^$HOME/||" | fzf
+)
+
+echo $SELECTED
+
+[[ $SELECTED ]] && SELECTED="$HOME/$SELECTED"
+
+[[ ! $SELECTED ]] && exit 0
+
+SELECTED_NAME=$(basename "$SELECTED" | tr . _)
+
+if ! tmux has-session -t "$SELECTED_NAME"; then
+    tmux new-session -ds "$SELECTED_NAME" -c "$SELECTED"
+    tmux select-window -t "$SELECTED_NAME:1"
+fi
+
+tmux switch-client -t "$SELECTED_NAME"
+
